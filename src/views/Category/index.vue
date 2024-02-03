7e2fc87ac6dba066d54cccb5e38314b6acd80757
HildaM <zhao163frozen@gmail.com>
2024-02-03 14:45:51 +0800

    Day03: 一级分类面包屑导航渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { getCategoryAPI } from '@/apis/category'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+// 获取数据
+const categoryData = ref({})
+const route = useRoute()   // 在组件内部获取路由参数
+const getCategory = async () => {
+    const res = await getCategoryAPI(route.params.id)
+    categoryData.value = res.result
+}
+onMounted(() => getCategory())
+
+</script>
+
 <template>
-    <div>分类页</div>
+    <div class="top-category">
+        <!-- 面包屑 -->
+        <div class="bread-container">
+            <el-breadcrumb separator=">">
+                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+    </div>
 </template>
