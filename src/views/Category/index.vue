a51fe275e8936fcc0aac8511169788bf804ee8d6
HildaM <zhao163frozen@gmail.com>
2024-02-03 16:08:10 +0800

    Day03: 处理路由缓存问题

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,16 +2,24 @@
 import { getCategoryAPI } from '@/apis/category'
 import { getBannerAPI } from '@/apis/home'
 import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, onBeforeRouteUpdate } from 'vue-router'
+import GoodsItem from '../Home/components/GoodItem.vue'
+
 
 // 获取数据
 const categoryData = ref({})
 const route = useRoute()   // 在组件内部获取路由参数
-const getCategory = async () => {
-    const res = await getCategoryAPI(route.params.id)
+const getCategory = async (id = route.params.id) => {   // 默认参数
+    const res = await getCategoryAPI(id)
     categoryData.value = res.result
 }
 onMounted(() => getCategory())
+
+// 路由参数变化后，将分类数据重新请求
+onBeforeRouteUpdate((to) => {
+    // 使用最新的路由参数请求最新的分类数据
+    getCategory(to.params.id)
+})
 
 // 获取Banner
 const bannerList = ref([])
@@ -45,12 +53,111 @@ onMounted(() => getBanner())
                     </el-carousel-item>
                 </el-carousel>
             </div>
+
+            <div class="sub-list">
+                <h3>全部分类</h3>
+                <ul>
+                    <li v-for="i in categoryData.children" :key="i.id">
+                        <RouterLink to="/">
+                            <img :src="i.picture" />
+                            <p>{{ i.name }}</p>
+                        </RouterLink>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+                <div class="head">
+                    <h3>- {{ item.name }}-</h3>
+                </div>
+                <div class="body">
+                    <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-// 部分代码省略
+.top-category {
+    h3 {
+        font-size: 28px;
+        color: #666;
+        font-weight: normal;
+        text-align: center;
+        line-height: 100px;
+    }
+
+    .sub-list {
+        margin-top: 20px;
+        background-color: #fff;
+
+        ul {
+            display: flex;
+            padding: 0 32px;
+            flex-wrap: wrap;
+
+            li {
+                width: 168px;
+                height: 160px;
+
+
+                a {
+                    text-align: center;
+                    display: block;
+                    ;
+                    font-size: 16px;
+
+                    img {
+                        width: 100px;
+                        height: 100px;
+                    }
+
+                    p {
+                        line-height: 40px;
+                    }
+
+                    &:hover {
+                        color: $xtxColor;
+                    }
+                }
+            }
+        }
+    }
+
+    .ref-goods {
+        background-color: #fff;
+        margin-top: 20px;
+        position: relative;
+
+        .head {
+            .xtx-more {
+                position: absolute;
+                top: 20px;
+                right: 20px;
+            }
+
+            .tag {
+                text-align: center;
+                color: #999;
+                font-size: 20px;
+                position: relative;
+                top: -20px;
+            }
+        }
+
+        .body {
+            display: flex;
+            justify-content: space-around;
+            padding: 0 40px 30px;
+        }
+    }
+
+    .bread-container {
+        padding: 25px 0;
+    }
+}
+
 .home-banner {
     width: 1240px;
     height: 500px;
